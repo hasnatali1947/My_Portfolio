e390037fb5c7ba1f46485e33aef801843d587782
HASNAT KHAN <Hasnatking1947@gmail.com>
2023-08-28 19:16:17 +0500

    added landing page

---
+++ portfolio/app/NameScroll.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const names = ['Name 1', 'Name 2', 'Name 3', 'Name 4']; // Add your names here
+
+const NameScroll = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % names.length);
+    }, 1000); // Change the interval to control the scrolling speed (milliseconds)
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {names.map((name, index) => (
+          <div key={index}>{name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NameScroll;
---
+++ portfolio/app/page.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+// import { useClient } from 'next/client';
+import NameScroll from "./NameScroll";
+
+const Page = () => {
+  // useClient();
+  return(
+    <>
+    <header>
+      <nav>
+        <ul className="headerLeft">
+          <li>gmail
+          {/* <Image src="/icons/Gmail.svg" alt="Gmail Logo" width={50} height={50} /> */}
+          </li>
+          <li>linkedIn</li>
+          <li>github</li>
+          </ul>
+          <ul className="headerRight">
+            <li>landing</li>
+            <li>projects</li>
+            <li>about me</li>
+            <li>contact</li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section className="landing_page">
+
+        <h3>Hi, I'm Hasnat Ali</h3>
+        <h1>Building digital <br /> products,brands,and <br /> experience.</h1>
+
+        <p>a Product Designer and Visual Developer in SF. <br /> Ispeciallize in UI/UX Design, Responsive Web Design, <br /> and Visual Development</p>
+        <button>CONNECT WITH ME</button>
+      </section>
+
+      <section className="projects">
+        <h2>My Projects</h2>
+        <p>witness the beauty of through our lens, <br /> as we showcase stunning landscaps that evoke <br /> wonder and appreciation for the environment.</p>
+      
+        <button>Left</button>
+        <button>Right</button>
+        <div>
+        <NameScroll />
+        </div>
+
+      </section>
+
+    </main>
+    </>
+  )
+}
+
+export default Page;
